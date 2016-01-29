78f09760c2541d7349e350a844285e3763fcdebc
Your Name <you@example.com>
2016-01-29 23:32:52 +0800

    增加iostream测试

--- 4/class_test/main.cpp
+++ 4/class_test/main.cpp
@@ -4,7 +4,16 @@ using namespace std;
 
 int main()
 {
+    int carrots;
+    cout << "How many carrots do you have?" << endl;
+    cin >> carrots;
+    cout << "Here are tow more.";
+    carrots = carrots * 2;
+    //the next line concatenates output
+    cout << "Now you have " << carrots << " carrots." << endl;
     cout << "Hello World!" << endl;
+    cerr << "Here happens some err!" << endl;
+    clog << "Record some logs" << endl;
     return 0;
 }
 
